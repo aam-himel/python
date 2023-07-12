87260f6b9309b7fe06f7a149f983ac62b0d5caec
aam-himel <aam.himel@gmail.com>
2023-07-12 07:44:41 +0600

    basics

---
+++ basics/basiscs.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+string_var = 3
+print("hello world", string_var)
+
+# The input() Function
+
+print("What is your name?")
+username = input()
+print("Hi, {}".format(username))
